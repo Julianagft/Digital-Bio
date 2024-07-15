f1a54cfa851c23e9fff4192aad1aeb51d3e1b9de
Cauan Victor <cauanvictor0325@gmail.com>
2024-07-15 14:15:44 -0300

    feat: Adicionando correção no build do frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
 function App() {
   return (
-    <Router>
-      <Switch>
+    <BrowserRouter>
+      <Routes>
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-      </Switch>
-    </Router>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
