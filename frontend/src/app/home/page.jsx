a556d3e3833ca78aa76d31c9ee25d0274e34e137
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-07-19 18:26:01 -0300

    montando a estrutura do front

---
+++ frontend/src/app/home/page.jsx
@@ -0,0 +1,8 @@
+export default function Home () {
+    
+    return (
+        <div className="bg-blue-300 h-full w-full">
+            <p>HOME</p>
+        </div>
+    );
+}
---
+++ frontend/src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="w-full h-auto bg-red-600">
+      <p>LOGIN</p>
+    </div>
+  );
+}
