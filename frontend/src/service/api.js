7885006736a4d4f891e96a6fce2a679dda27fb42
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-07-25 20:14:49 -0300

    endereço do back mudou

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-//   baseURL: "https://projeto-final-fs24.onrender.com/",
+//   baseURL: "https://projeto-final-fs24-yqy0.onrender.com/",
   baseURL: "http://localhost:8081",
   headers: {
     "Content-Type": "application/json", 
