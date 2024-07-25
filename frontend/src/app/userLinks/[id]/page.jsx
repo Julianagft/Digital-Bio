799b313536935dd8eb4ec83f61556d89ef920df4
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-07-25 18:42:49 -0300

    jshdsjh

--- frontend/src/app/userLinks/[id]/page.jsx
+++ frontend/src/app/userLinks/[id]/page.jsx
@@ -1,4 +1,8 @@
 export default function userLinksPage () {
+
+    return (
+        
+    )
     // return (
     //     <div key={link.id} className="font-bold text-[#1e3a8a] flex justify-center
     //     border-[1px] shadow-md mb-6 py-5 w-[70%]">
