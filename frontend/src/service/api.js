69acfcde51aa74e5331532fab5b11d3aca90c9dd
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-07-25 21:37:14 -0300

    new port API

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-//   baseURL: "https://projeto-final-fs24-yqy0.onrender.com/",
-  baseURL: "http://localhost:8081",
+  baseURL: "https://projeto-final-fs24-yqy0.onrender.com/",
+  // baseURL: "http://localhost:8081",
   headers: {
     "Content-Type": "application/json", 
   }
